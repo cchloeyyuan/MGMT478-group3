8c4e572027b40f56a46656f82effdf6fe21e167f
Chloe Y <chloeyuan@pal-nat186-158-248.itap.purdue.edu>
2024-01-26 15:54:53 -0500

    comments

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 for _ in range(10):
     print('Test')
+
+#  Add process
+# Step 1: git add . > which means add everything you changed on server
+# Step 2: git commit -m "comment" > Add comment on the main branch
+# Step 3: git push > upload on github
+    
+
+    
