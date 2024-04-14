e8553d9d564d2ba4bf85f54eea948f0e0912f552
Caleb Hammoudeh <70403898+CalebHammoudeh@users.noreply.github.com>
2024-04-14 12:07:39 -0400

    Added Group 2's Lasso Prediction Function

--- project/worldmap/views.py
+++ project/worldmap/views.py
@@ -20,8 +20,11 @@ from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import warnings
-from shapely.geometry import Polygon
 from .forms import TimePeriodForm
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LassoCV
+from sklearn.metrics import mean_squared_error
+from sklearn.exceptions import ConvergenceWarning
 
 
 def map_view(request):
@@ -224,3 +227,146 @@ def time_period_request(request):
         'form': form,
         'data': data_for_period
     })
+
+def lasso_prediction(data):
+    data['DATE'] = pd.to_datetime(data['DATE'])
+    # Extract year and month from DATE as new features
+    data['YEAR'] = data['DATE'].dt.year
+    data['MONTH'] = data['DATE'].dt.month
+    # Drop the 'SNOW' column
+    data_cleaned = data.drop(['SNOW'], axis=1)
+    # Convert non-numeric to numeric
+    for column in ['LATITUDE','LONGITUDE','ELEVATION','AWND', 'TAVG', 'TMAX', 'TMIN']:
+        data_cleaned[column] = pd.to_numeric(data_cleaned[column], errors='coerce')
+    # Imputer missing data as median of the column
+    imputer = SimpleImputer(strategy='median')
+    data_cleaned[['AWND', 'TAVG', 'TMAX', 'TMIN']] = imputer.fit_transform(data_cleaned[['AWND', 'TAVG', 'TMAX', 'TMIN']])
+    stations = data_cleaned[['STATION', 'NAME', 'LATITUDE', 'LONGITUDE']].drop_duplicates()
+    coordinates = stations[['LATITUDE', 'LONGITUDE']]
+    neighbors_model = NearestNeighbors(n_neighbors=6)
+    neighbors_model.fit(coordinates)
+
+    #Get six nearest weather stations around a given specific weather station
+    def six_nearest_weather_stations(latitude, longitude):
+        query_coordinates = np.array([[latitude, longitude]])
+        distances, indices = neighbors_model.kneighbors(query_coordinates)
+        nearest_stations_info = stations.iloc[indices[0]].copy() 
+        nearest_stations_info['DISTANCE(°)'] = distances[0]
+
+        return nearest_stations_info
+    
+    #Get the average value for the nearest weather station, exclude the one used as predicted weather station
+    def average_values_for_nearest_stations_exclude(latitude, longitude, station_to_exclude):
+        nearest_stations_info = six_nearest_weather_stations(latitude, longitude)
+        nearest_station_ids = nearest_stations_info['STATION'].tolist()
+        
+        # Remove the specific station ID from the list
+        if station_to_exclude in nearest_station_ids:
+            nearest_station_ids.remove(station_to_exclude)
+        
+        filtered_data = data_cleaned[data_cleaned['STATION'].isin(nearest_station_ids)]
+        average_values = filtered_data.groupby(['YEAR', 'MONTH'])[['AWND', 'PRCP', 'TAVG', 'TMAX', 'TMIN']].mean().reset_index()
+        average_values.rename(columns={
+            'AWND': 'AWND_avg',
+            'PRCP': 'PRCP_avg',
+            'TAVG': 'TAVG_avg',
+            'TMAX': 'TMAX_avg',
+            'TMIN': 'TMIN_avg'
+        }, inplace=True)
+        return average_values
+    
+    #Expand the dataset
+    def get_analysis_data(latitude, longitude, weather_station):
+        weather_station_data = data_cleaned[data_cleaned['STATION']== weather_station]
+        
+        for var in ['AWND', 'PRCP', 'TAVG', 'TMAX', 'TMIN']:
+            for year in range(1, 6):
+                year_lag = year*12
+                weather_station_data[f'{var}_lag_{year}_year'] = weather_station_data[var].shift(year_lag)
+                
+        neighbor_data = average_values_for_nearest_stations_exclude(latitude, longitude, weather_station)
+        merged_data = pd.merge(weather_station_data, neighbor_data, on=['YEAR', 'MONTH'], how='inner')
+        
+        for var in ['AWND_avg', 'PRCP_avg', 'TAVG_avg', 'TMAX_avg', 'TMIN_avg']:
+            for year in range(1, 6):
+                year_lag = year*12
+                merged_data[f'{var}_lag_{year}_year'] = merged_data[var].shift(year_lag)
+        
+        merged_data_final = merged_data.drop(columns=['AWND', 'TAVG', 'TMAX', 'TMIN', 'AWND_avg', 'PRCP_avg', 'TAVG_avg', 'TMAX_avg', 'TMIN_avg'])
+        merged_data_final = merged_data_final.dropna()
+        return merged_data_final
+    
+    #Lasso - Top 5 Feature Selection
+    def lasso_mse(latitude, longitude, weather_station):
+        
+        merged_data_final = get_analysis_data(latitude, longitude, weather_station)
+        
+        # Initialize dictionaries to store MSE values for each year and top features for each year
+        mse_values = {}
+        top_features_per_year = {}
+
+        # Initialize lists to accumulate actual and predicted values for all years
+        all_actuals = []
+        all_predictions = []
+
+        # Set the starting and ending years for the time window
+        years = merged_data_final['YEAR'].unique()
+        start_year = years[0] + 4
+        end_year = 2022
+
+        # Loop through each time window
+        for year in range(start_year, end_year + 1):
+            # Define the training and testing sets
+            train_df = merged_data_final[merged_data_final['YEAR'].between(year - 4, year)]
+            test_df = merged_data_final[merged_data_final['YEAR'] == year + 1]
+
+            # Remove rows with missing values
+            train_df = train_df.dropna()
+            test_df = test_df.dropna()
+
+            # Select features and target variable
+            X_train = train_df.drop(columns=['STATION', 'NAME', 'LATITUDE', 'LONGITUDE', 'ELEVATION', 'DATE', 'PRCP', 'YEAR'])
+            y_train = train_df['PRCP']
+            X_test = test_df[X_train.columns]
+            y_test = test_df['PRCP']
+
+            # Use LassoCV for feature selection and model fitting
+            lasso = LassoCV(cv=5).fit(X_train, y_train)
+
+            # Predict using the model
+            y_pred = lasso.predict(X_test)
+            # Calculate and store the MSE value
+            mse = mean_squared_error(y_test, y_pred)
+            mse_values[year] = mse
+
+            # Accumulate actual and predicted values for all years
+            all_actuals.extend(y_test.tolist())
+            all_predictions.extend(y_pred.tolist())
+
+            # Get feature importance and store the top 5 features for the year
+            feature_importance = np.abs(lasso.coef_)
+            feature_names = X_train.columns
+            features_coef = zip(feature_names, feature_importance)
+            top_features = sorted(features_coef, key=lambda x: x[1], reverse=True)[:5]
+            top_features_per_year[year] = top_features
+
+        # After looping, calculate the overall MSE
+        overall_mse = mean_squared_error(all_actuals, all_predictions)
+        
+        return overall_mse
+
+    warnings.filterwarnings("ignore", category=ConvergenceWarning)
+    six_nearest_weather_stations(40.41236,-86.94739)
+    lasso_mse(40.41236,-86.94739,'USW00014835') # Purdue Airport
+    station_counts = data_cleaned.groupby('STATION').size()
+
+    #Loop all Valid Weather Stations
+    #Filter weather stations if it has records from 2010 to 2023
+    stations_with_enough_records = station_counts[station_counts >= 167].index
+    valid_stations = data_cleaned[
+        (data_cleaned['YEAR'] == 2010) & 
+        (data_cleaned['STATION'].isin(stations_with_enough_records))
+    ][['STATION', 'NAME', 'LATITUDE', 'LONGITUDE']].drop_duplicates()
+    valid_stations['overall_mse'] = valid_stations.apply(lambda row: lasso_mse(row['LATITUDE'], row['LONGITUDE'], row['STATION']), axis=1)
+
+    
